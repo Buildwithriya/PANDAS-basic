17e1a7a081c18640b3180b1b68381a668d29b4a5
riya <riya26082006@gmail.com>
2026-05-14 22:09:34 +0530

    logic handling and cleaning the data

---
+++ 6. mini project - data cleaning (python)/cleaned data.py
@@ -0,0 +1,80 @@
+
+import pandas as pd 
+import numpy as np
+
+df= pd.read_csv(r"C:\Users\Riya\OneDrive\Documents\PANDAS basic\6. mini project - data cleaning (python)\dirty_cafe_sales.csv")
+
+print("shape:",df.shape)
+print(df.head())
+print (df.info())
+print(df.describe())
+
+# null value check 
+
+print(df.isnull().sum())
+
+# unique value in columns
+
+print(df['Item'].unique().value_counts())
+print(df['Payment Method'].unique())
+print(df['Location'].unique())
+
+
+# handling missing values
+
+df['Location'] = df['Location'].replace(['ERROR', '', np.nan], 'Takeaway')
+print (df['Location'].unique())
+
+
+df['Payment Method'] = df['Payment Method'].replace(['ERROR', '', np.nan], 'Digital Wallet')
+print (df['Payment Method'].unique())
+
+df['Item'] = df['Item'].replace(['ERROR', '', np.nan], 'UNKNOWN')
+print (df['Item'].unique())
+
+df['Quantity'] = df['Quantity'].replace(['ERROR','UNKNOWN', ''], np.nan)
+print (df['Quantity'].unique())
+
+df['Price Per Unit'] = df['Price Per Unit'].replace(['ERROR','UNKNOWN', ''], np.nan)
+print (df['Price Per Unit'].unique())
+
+df['Total Spent'] = df['Total Spent'].replace(['ERROR','UNKNOWN', ''], np.nan)
+print (df['Total Spent'].unique())
+
+
+df['Transaction Date'] = df['Transaction Date'].replace(['ERROR', '', np.nan], 'UNKNOWN')
+print (df['Transaction Date'].unique())
+
+
+
+# change data type 
+
+
+df['Price Per Unit'] = df['Price Per Unit'].astype(float)
+
+df['Total Spent'] = df['Total Spent'].astype(float)
+
+print(df.info())
+
+
+cols_to_check = ['Quantity', 'Total Spent', 'Price Per Unit']
+for col in cols_to_check:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+
+df = df.dropna(subset=cols_to_check, thresh=2)
+# # logic handling 
+
+df['Quantity'] = df['Quantity'].fillna(df['Total Spent'] / df['Price Per Unit'])
+
+df['Total Spent'] = df['Total Spent'].fillna(df['Quantity'] * df['Price Per Unit'])
+
+df['Price Per Unit'] = df['Price Per Unit'].fillna(df['Total Spent'] / df['Quantity'])
+
+df['Quantity'] = df['Quantity'].round().astype('Int64')
+df['Total Spent'] = df['Total Spent'].round().astype('Float64')
+df['Price Per Unit'] = df['Price Per Unit'].round().astype('Float64')
+
+
+print(df.info())
+
+print (df.head(240))
